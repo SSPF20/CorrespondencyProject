a22a9b6538c24b40592ab6cf983422e8ca098594
hgp691 <hgp691@gmail.com>
2023-09-19 18:10:16 -0500

    Initial Commit

---
+++ ViewSI/ViewSIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewSIApp.swift
+//  ViewSI
+//
+//  Created by Horacio Parra Rodriguez on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
