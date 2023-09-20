fc30b302f7ce78d3d27ebe4031ba99ad2609ad12
hgp691 <hgp691@gmail.com>
2023-09-19 20:14:52 -0500

    Add view

--- ViewSI/ContentView.swift
+++ ViewSI/ContentView.swift
@@ -8,17 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var title = "Hey"
+    @State var text = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack() {
+                    NameTextField(name: NSLocalizedString("Nombre del usuario:", comment: ""),
+                                  textFieldTitle:  NSLocalizedString("Nombre", comment: ""))
+                    NameTextField(name: NSLocalizedString("Cédula:", comment: ""),
+                                  textFieldTitle: NSLocalizedString("Número", comment: ""))
+                    NameTextField(name: NSLocalizedString("Correo electrónico:", comment: ""),
+                                  textFieldTitle: NSLocalizedString("Correo", comment: ""))
+                    NameTextField(name: NSLocalizedString("Celular:", comment: ""),
+                                  textFieldTitle: NSLocalizedString("Número", comment: ""))
+                    NameTextField(name: NSLocalizedString("Fecha de recibido:", comment: ""),
+                                  textFieldTitle: NSLocalizedString("Fecha", comment: ""))
+                    NameTextField(name: NSLocalizedString("Fecha de entrega:", comment: ""),
+                                  textFieldTitle: NSLocalizedString("Fecha", comment: ""))
+                    NameTextField(name: NSLocalizedString("Correo electrónico:", comment: ""),
+                                  textFieldTitle: NSLocalizedString("Correo", comment: ""))
+                    NameTextField(name: NSLocalizedString("Caso:", comment: ""),
+                                  textFieldTitle: NSLocalizedString("Caso", comment: ""))
+                    NameTextField(name: NSLocalizedString("Departamento asignado:", comment: ""),
+                                  textFieldTitle: NSLocalizedString("Departamento", comment: ""))
+                    NameTextField(name: NSLocalizedString("Abogado asignado", comment: ""),
+                                  textFieldTitle: NSLocalizedString("Nombre", comment: ""))
+                    NameTextField(name: NSLocalizedString("Resumen:", comment: ""), textFieldTitle: NSLocalizedString("", comment: ""), axis: .vertical, height: 300)
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "square.and.arrow.down")
+                            .font(.system(size: 50))
+                    }
+                }
+                .navigationTitle(Text(NSLocalizedString("Correspondencia", comment: "")))
+                .toolbarBackground(Color.yellow, for: .navigationBar)
+                .padding()
+            }
+            
         }
-        .padding()
+        
     }
 }
 
+
+struct NameTextField: View {
+    @State var text = ""
+    
+    var name: String
+    var textFieldTitle: String
+    var axis: Axis = .horizontal
+    var height: CGFloat = 40
+    
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text(name)
+                .font(.headline)
+                .foregroundColor(.primary)
+            TextField(NSLocalizedString(textFieldTitle, comment: ""), text: $text, axis: axis)
+                .frame(height: height)
+                .background(.tertiary)
+                .multilineTextAlignment(.leading)
+                .cornerRadius(16)
+        }
+        .padding(.top)
+    }
+    
+}
 #Preview {
     ContentView()
 }
